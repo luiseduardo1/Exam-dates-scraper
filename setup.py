6617db783c752741be315858b694deb8c996beab
Luis E. Obando <luiseduardo.obando@gmail.com>
2015-10-18 02:09:24 -0400

    Correct setup.py

--- setup.py
+++ setup.py
@@ -11,24 +11,29 @@ except ImportError:
 version = '1.0'
 
 with open('README.md') as readme_file:
-    readme = read_file.read()
+    readme = readme_file.read()
 
 requirements = [
-        'Beautifulsoup4>=4.4.1',
-        'Mechanize==0.2.5',
+        'beautifulsoup4>=4.4.1',
+        'mechanize==0.2.5',
         ]
 
 long_description = readme
+
 setup(
     name = 'ExamsDatesScraper',
     version = version,
-    description = 'A program to recuperate all your exams/homeworks dates 
-                    from your Pixel/Laval university account',
+    description = 'A program to recuperate all your exams/homeworks dates' 
+                    'from your Pixel/Laval university account',
     long_description = long_description,
     url = 'http://github.com/luiseduardo1/ExamsDatesScraper',
     author = 'Luis Eduardo Obando',
-    author_email = 'luiseduardo.obando@gmail.com'
+    author_email = 'luiseduardo.obando@gmail.com',
     license = 'MIT',
     packages = find_packages(),
+    entry_points = {
+        'console_scripts': [
+            'ExamsDatesScraper = ExamsDatesScraper:evaluationsScraper'],
+        },
     install_requires = requirements 
 )
